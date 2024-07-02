b526501ced06e463bcce8bd0e0ad09a909437b9c
miltonws <milton.willamis.silva@gmail.com>
2024-07-01 22:07:43 -0300

    fix: refactor nome da pasta main

--- src/main/java/com/ms/gerenciador/produtos/GerenciadorProdutosApplication.java
+++ src/main/java/com/ms/gerenciador/produtos/GerenciadorProdutosApplication.java
@@ -1,4 +1,4 @@
-package com.ms.gerenciador.produtos.gerenciador_produtos;
+package com.ms.gerenciador.produtos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ms/gerenciador/produtos/GerenciadorProdutosApplicationTests.java
+++ src/test/java/com/ms/gerenciador/produtos/GerenciadorProdutosApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ms.gerenciador.produtos.gerenciador_produtos;
+package com.ms.gerenciador.produtos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
