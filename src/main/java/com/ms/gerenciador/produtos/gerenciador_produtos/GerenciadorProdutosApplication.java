5919b6a1567039e16dd302c4532f35a9f36ad9c2
miltonws <milton.willamis.silva@gmail.com>
2024-07-01 21:52:04 -0300

    subindo estrutura inicial projeto produto

---
+++ src/main/java/com/ms/gerenciador/produtos/gerenciador_produtos/GerenciadorProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.ms.gerenciador.produtos.gerenciador_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/gerenciador/produtos/gerenciador_produtos/GerenciadorProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.gerenciador.produtos.gerenciador_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
